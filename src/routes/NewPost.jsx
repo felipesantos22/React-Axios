b73269d11cb672bd30e4d1c24f80a85e2fd52b20
felipesantos22 <felipevs2018@gmail.com>
2022-11-26 07:11:07 -0300

    css

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./NewPost.css"
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+    <div className='new-post'>
+        <h2>Inserir novo post:</h2>
+        <form>
+        <div className="form-control">
+            <label htmlFor="title">Título</label>
+            <input type="text" name="title" id="title" placeholder='Digite o título' />
+        </div>
+        <div className="form-control">
+            <label htmlFor="body">Contéudo</label>
+            <textarea type="text" name="title" id="title" placeholder='Digite o contéudo' ></textarea>
+        </div>
+        <input type="submit" value="Criar Post" className='btn' />
+        </form>
+    </div>
   )
 }
 
