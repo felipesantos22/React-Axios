cdf94fdfaf6b7e94ad727b9fe92ea48c546955f6
felipesantos22 <felipevs2018@gmail.com>
2022-11-26 05:33:35 -0300

    first commit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import './Home.css'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const [post, setPost] = useState([])
+
+    const getPost = async () => {
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+                // fetch('https://jsonplaceholder.typicode.com/posts')
+                //     .then((response) => response.json())
+                //     .then((json) => console.log(json));                
+                const data = response.data;
+                console.log(data)
+        } catch (error) {
+           console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getPost();
+    }, [])
+
+    return (
+        <div>Home</div>
+    )
 }
 
 export default Home
